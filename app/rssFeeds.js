4cfc0deb34f6a28de336df6adabd8360c3937e93
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2024-10-13 21:56:17 +0000

    added new providers

--- app/rssFeeds.js
+++ app/rssFeeds.js
@@ -5,5 +5,7 @@ export const rssFeeds = [
     {name: "Apple Machine Learning Research", slug: "apple-machine-learning-research", url: "https://machinelearning.apple.com/rss.xml"},
     {name: "9to5mac", slug: "9-to-5mac", url: "https://9to5mac.com/feed/"},
     {name: "Techcrunch", slug: "techcrunch", url: "https://techcrunch.com/feed/"},
+    {name: "3blue1brown", slug: "3blue1brown", url: "https://3blue1brown.substack.com/feed"},
+    {name: "Reddit", slug: "reddit", url: "https://www.reddit.com/r/programming.rss"}
   ];
   
