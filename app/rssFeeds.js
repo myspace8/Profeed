16ece8f7f30b59431ece1c3378628b1912b802d8
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2024-10-18 17:31:47 +0000

    Added mit tech review

--- app/rssFeeds.js
+++ app/rssFeeds.js
@@ -1,10 +1,10 @@
 
 export const rssFeeds = [
-    { name: "Hacker News", slug: "hacker-news", url: "https://hnrss.org/frontpage" },
-    {name: "Paul Graham", slug: "paul-graham", url: "http://www.aaronsw.com/2002/feeds/pgessays.rss"},
-    {name: "Apple Machine Learning Research", slug: "apple-machine-learning-research", url: "https://machinelearning.apple.com/rss.xml"},
-    {name: "Discover magazine", slug: "Dicover", url: "http://feeds.feedburner.com/discovercrux"},
-    {name: "9to5mac", slug: "9-to-5mac", url: "https://9to5mac.com/feed/"},
-    {name: "Techcrunch", slug: "techcrunch", url: "https://techcrunch.com/feed/"},
-  ];
-  
+  { name: "MIT Technology Review", slug: "MIT-Technology-Review", url: "https://www.technologyreview.com/feed" },
+  { name: "Hacker News", slug: "hacker-news", url: "https://hnrss.org/frontpage" },
+  { name: "Paul Graham", slug: "paul-graham", url: "http://www.aaronsw.com/2002/feeds/pgessays.rss" },
+  { name: "Apple Machine Learning Research", slug: "apple-machine-learning-research", url: "https://machinelearning.apple.com/rss.xml" },
+  { name: "Discover magazine", slug: "Dicover", url: "http://feeds.feedburner.com/discovercrux" },
+  { name: "9to5mac", slug: "9-to-5mac", url: "https://9to5mac.com/feed/" },
+  { name: "Techcrunch", slug: "techcrunch", url: "https://techcrunch.com/feed/" },
+];
