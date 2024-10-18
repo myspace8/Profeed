b2b687520a2172ef38ee01b095443abd783c7a68
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2024-10-18 05:32:13 +0000

    added "Discover magazine

--- app/rssFeeds.js
+++ app/rssFeeds.js
@@ -3,7 +3,7 @@ export const rssFeeds = [
     { name: "Hacker News", slug: "hacker-news", url: "https://hnrss.org/frontpage" },
     {name: "Paul Graham", slug: "paul-graham", url: "http://www.aaronsw.com/2002/feeds/pgessays.rss"},
     {name: "Apple Machine Learning Research", slug: "apple-machine-learning-research", url: "https://machinelearning.apple.com/rss.xml"},
-    {name: "Lex Freidman Podcast", slug: "lex-freidman-podcast", url: "https://lexfridman.com/feed"},
+    {name: "Discover magazine", slug: "Dicover", url: "http://feeds.feedburner.com/discovercrux"},
     {name: "9to5mac", slug: "9-to-5mac", url: "https://9to5mac.com/feed/"},
     {name: "Techcrunch", slug: "techcrunch", url: "https://techcrunch.com/feed/"},
   ];
