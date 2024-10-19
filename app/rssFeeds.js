cd9cf7f5cf4d1aa5cbeff583fee9ad3b53d14cce
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2024-10-19 11:13:28 +0000

    added provider

--- app/rssFeeds.js
+++ app/rssFeeds.js
@@ -1,10 +1,11 @@
 
 export const rssFeeds = [
-  { name: "MIT Technology Review", slug: "MIT-Technology-Review", url: "https://www.technologyreview.com/feed" },
+  { name: "MIT Technology Review", slug: "mit-technology-review", url: "https://www.technologyreview.com/feed" },
+  {name: "Narrative Magazine", slug: "narrative-magazine", url: "https://www.narrativemagazine.com/rss.xml"},
+  { name: "Discover magazine", slug: "dicover-magazine", url: "http://feeds.feedburner.com/discovercrux" },
+  { name: "Techcrunch", slug: "techcrunch", url: "https://techcrunch.com/feed/" },
   { name: "Hacker News", slug: "hacker-news", url: "https://hnrss.org/frontpage" },
   { name: "Paul Graham", slug: "paul-graham", url: "http://www.aaronsw.com/2002/feeds/pgessays.rss" },
   { name: "Apple Machine Learning Research", slug: "apple-machine-learning-research", url: "https://machinelearning.apple.com/rss.xml" },
-  { name: "Discover magazine", slug: "Dicover", url: "http://feeds.feedburner.com/discovercrux" },
   { name: "9to5mac", slug: "9-to-5mac", url: "https://9to5mac.com/feed/" },
-  { name: "Techcrunch", slug: "techcrunch", url: "https://techcrunch.com/feed/" },
 ];
