8846bd355267c3f1d5eca46d8dd8a522b4ae1085
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2024-10-21 15:31:39 +0000

    added few providers

--- app/rssFeeds.js
+++ app/rssFeeds.js
@@ -1,11 +1,21 @@
 
 
+// { name: "Hacker News", slug: "hacker-news", url: "https://hnrss.org/frontpage" },
+// {name: "Apple Machine Learning Research", slug: "apple-machine-learning-research", url: "https://machinelearning.apple.com/rss.xml"},
+// {name: "9to5mac", slug: "9-to-5mac", url: "https://9to5mac.com/feed/"},
+// {name: "Techcrunch", slug: "techcrunch", url: "https://techcrunch.com/feed/"},
+// {name: "3blue1brown", slug: "3blue1brown", url: "https://3blue1brown.substack.com/feed"},
+// ];
 export const rssFeeds = [
-    { name: "Hacker News", slug: "hacker-news", url: "https://hnrss.org/frontpage" },
-    {name: "Apple Machine Learning Research", slug: "apple-machine-learning-research", url: "https://machinelearning.apple.com/rss.xml"},
-    {name: "9to5mac", slug: "9-to-5mac", url: "https://9to5mac.com/feed/"},
-    {name: "Techcrunch", slug: "techcrunch", url: "https://techcrunch.com/feed/"},
-    {name: "3blue1brown", slug: "3blue1brown", url: "https://3blue1brown.substack.com/feed"},
-    {name: "Reddit", slug: "reddit", url: "https://www.reddit.com/r/programming.rss"}
-  ];
   
+  {name: "Reddit", slug: "reddit", url: "https://www.reddit.com/r/programming.rss"},
+  { name: "MIT Technology Review", slug: "mit-technology-review", url: "https://www.technologyreview.com/feed" },
+  {name: "Narrative Magazine", slug: "narrative-magazine", url: "https://www.narrativemagazine.com/rss.xml"},
+  { name: "Discover magazine", slug: "dicover-magazine", url: "http://feeds.feedburner.com/discovercrux" },
+  { name: "Techcrunch", slug: "techcrunch", url: "https://techcrunch.com/feed/" },
+  { name: "Hacker News", slug: "hacker-news", url: "https://hnrss.org/frontpage" },
+  { name: "Paul Graham", slug: "paul-graham", url: "http://www.aaronsw.com/2002/feeds/pgessays.rss" },
+  { name: "Apple Machine Learning Research", slug: "apple-machine-learning-research", url: "https://machinelearning.apple.com/rss.xml" },
+  { name: "9to5mac", slug: "9-to-5mac", url: "https://9to5mac.com/feed/" },
+];
+
